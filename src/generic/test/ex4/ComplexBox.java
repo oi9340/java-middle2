74be6715fd359d3b67e4555d331df2d7804c64ba
oi9340 <oi9340@naver.com>
2024-06-09 02:01:11 +0900

    Wildcard example

--- src/generic/test/ex4/ComplexBox.java
+++ src/generic/test/ex4/ComplexBox.java
@@ -1,4 +1,17 @@
 package generic.test.ex4;
 
-public class ComplexBox {
+import generic.animal.Animal;
+
+public class ComplexBox<T extends Animal> {
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public <Z> Z printAndReturn(Z z) {
+        System.out.println("animal.className : " + animal.getClass());
+        System.out.println("t.className : " + z.getClass().getName());
+        return z;
+    }
 }
---
+++ src/generic/test/ex5/WildCardMain2.java
@@ -0,0 +1,32 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildCardMain2 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        writeBox(objectBox);
+        writeBox(animalBox);
+//        writeBox(dogBox); // 하한이 Animal
+//        writeBox(catBox); // 하한이 Animal
+
+        Animal animal = animalBox.get();
+        System.out.println("animal = " + animal);
+    }
+
+
+    static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍멍이", 100));
+    }
+
+
+}
+
+
